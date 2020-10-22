f6448d0335ec4e46b9425ebbc4ae93697d49ceb3
yamazaki <51977859+naikasann@users.noreply.github.com>
2020-10-22 15:07:20 +0900

    :construction: 動画を1フレームごとに切り出す機能の改善

--- movie_1s.py
+++ movie_1s.py
@@ -1,36 +1,39 @@
 import cv2
-from os import makedirs
+from os import makedirs, startfile
 from os.path import splitext, dirname, basename, join
+import os
 
-def save_frames(video_path: str, frame_dir: str, name="image", ext="jpg"):
+foldername = "images"
+
+def save_frames(video_path, startnumber=0, ext="jpg"):
+    savenumber = startnumber
+
     cap = cv2.VideoCapture(video_path)
     if not cap.isOpened():
         return
-    v_name = splitext(basename(video_path))[0]
-    if frame_dir[-1:] == "\\" or frame_dir[-1:] == "/":
-        frame_dir = dirname(frame_dir)
-    frame_dir_ = join(frame_dir, v_name)
-
-    makedirs(frame_dir_, exist_ok=True)
-    base_path = join(frame_dir_, name)
-
     idx = 0
     while cap.isOpened():
         idx += 1
         ret, frame = cap.read()
         if ret:
             if cap.get(cv2.CAP_PROP_POS_FRAMES) == 1:  # 0秒のフレームを保存
-                cv2.imwrite("{}_{}.{}".format(base_path, "0000", ext),
-                            frame)
+                cv2.imwrite("/train/image_{}.{}".format(str(savenumber), ext),frame)
+                savenumber += 1
             elif idx < cap.get(cv2.CAP_PROP_FPS):
                 continue
             else:  # 1秒ずつフレームを保存
                 second = int(cap.get(cv2.CAP_PROP_POS_FRAMES)/idx)
                 filled_second = str(second).zfill(4)
-                cv2.imwrite("{}_{}.{}".format(base_path, filled_second, ext),
-                            frame)
+                cv2.imwrite("/train/{}.{}".format(str(savenumber), ext),frame)
+                savenumber += 1
                 idx = 0
         else:
             break
 
-save_frames("./gotiusa.mp4", ".\\frame")
+    return savenumber
+
+videoname = 0
+videolist = os.listdir(foldername)
+for video in videolist:
+    videopath = os.path.join(foldername, video)
+    videoname = save_frames(videopath, videoname)
